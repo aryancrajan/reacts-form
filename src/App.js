87e4b736c78d0f38b0e03102f2100fb8ae13a168
ARYAN C R <57360045+aryancrajan@users.noreply.github.com>
2023-08-14 16:56:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form Data:', formData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Simple React Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Message:
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/server.js
@@ -0,0 +1,30 @@
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/data', (req, res) => {
+    console.log(req.query)
+    const id = req.query.id;
+    const data = fs.readFileSync(id + '.json');
+    res.send(data);
+});
+
+app.post('/submit', (req, res) => {
+    const body = req.body;
+    console.log({body});
+    const id=uuidv4();
+    fs.writeFileSync(id+'.json', JSON.stringify(body));
+    res.send({success: true, id: id});
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
